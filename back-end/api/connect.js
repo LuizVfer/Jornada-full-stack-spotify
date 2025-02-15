9fb5fc6ab5c4640174cace9058d4b2e8d3c7c0b1
Luiz Fernando Vieira <fernandovieirasantos10@gmail.com>
2025-02-14 20:00:49 -0400

    ajuste final

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -1,14 +1,7 @@
-// JavaScript Assincrono
-// await async
-// Fullfilled
-import { MongoClient } from "mongodb";
+import {MongoClient} from "mongodb";
 
-const URI =
-  "mongodb+srv://fullstackjornada:qojI71xVU2aV8UKC@cluster0.v1qra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const URI = "mongodb+srv://fernandovieirasantos10:fernando@cluster0.7gdta.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-const client = new MongoClient(URI);
+const client = new MongoClient(URI)
 
-export const db = client.db("spotifyAula");
-// const songCollection = await db.collection("songs").find({}).toArray();
-
-// console.log(songCollection);
+export const db = client.db("spotifyAula")
--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,9 +1,7 @@
-// Fetch ou Axios
-import "dotenv/config"
+
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
-const URL =  NODE_ENV === 'development' ? "http://localhost:3001/api" : "/api";
+const URL = "https://jornada-full-stack-spotify.onrender.com/api" ;
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
@@ -11,4 +9,4 @@ const responseSongs = await axios.get(`${URL}/songs`);
 export const artistArray = responseArtists.data;
 export const songsArray = responseSongs.data;
 
-// console.log(responseArtists.data);
+
